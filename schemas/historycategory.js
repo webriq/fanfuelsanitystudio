c01006edae2d9b1bbf758150ab75cc5246996199
Mark Gaudiano <markgaudiano@Marks-iMac.local>
2019-08-19 17:43:37 +0800

    change history category schema

--- schemas/historycategory.js
+++ schemas/historycategory.js
@@ -9,6 +9,11 @@ export default {
 			type: "string"
 		},
 		{
+			name: "listid",
+			title: "List ID",
+			type: "string"
+		},
+		{
 			name: "description",
 			title: "Description",
 			type: "text"
