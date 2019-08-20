3c9256e7b51af7e0f6639a2ae801eca794253000
Mark Gaudiano <markgaudiano@Marks-iMac.local>
2019-08-20 16:23:05 +0800

    change link for preview page

--- resolveProductionUrl.js
+++ resolveProductionUrl.js
@@ -1,4 +1,4 @@
 export default function resolveProductionUrl(document) {
-	return `https://fanfuelhq.webriq.me/preview?post=${document._id}`;
+	return `https://fanfuelhq.com/preview?post=${document._id}`;
 	console.log(document._id);
 }
